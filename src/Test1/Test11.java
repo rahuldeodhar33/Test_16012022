8d631aa495d9e616da833388dc65991691324b0d
Rahul Deodhar <rahuldeodhar33@gmail.com>
2022-01-16 05:16:04 +0000

    first commit

---
+++ src/Test1/Test11.java
@@ -0,0 +1,13 @@
+package Test1;
+import java.util.*;
+public class Test11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ...!!!");
+		Date date = new Date();
+		System.out.print("Today Is : ");
+		System.out.print(date.toString());
+	}
+
+}
